8396ac8f745d13159ef5cc09f8c0a8b59ddc2ae2
ayushsuhane <ayush.suhane@gmail.com>
2018-05-20 13:00:14 -0700

    Only uniform distribution for orthogonal boxes

--- Notebooks/initialization.py
+++ Notebooks/initialization.py
@@ -1,11 +1,7 @@
 import numpy as np
 from MDAnalysis.lib.distances import _box_check
 
-def init_uniform(box,Npoints):
-	boxtype = _box_check(box)
-	if boxtype == 'ortho': 
-		box1 = box[:3] if box.shape == (6,) \
-		else np.array((box[0][0],box[1][1],box[2][2]))
-	points = np.random.uniform(low=0,high=1.0,size=(Npoints,3))*box1[:]
+def init_uniform(box,Npoints,constrain=None):
+	points = np.random.uniform(low=0,high=1.0,size=(Npoints,3))*box[:3]
 	points = np.array(points,dtype=np.float32)
-	return box1,points
+	return points
